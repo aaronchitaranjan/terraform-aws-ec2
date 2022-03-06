be5ee546ab362be8fc9bd46fe3f39a6db8014fb7
Aaron Chitaranjan <Aaron.Chitaranjan@team.telstra.com>
2022-03-07 00:40:37 +1100

    update

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "this" {
     
     ami = data.aws_ami.ubuntu.id
     instance_type = var.instance_type
-    subnet = var.subnet_id
+    subnet_id = var.subnet_id
 
     tags = {
         Name = var.instance_name,
